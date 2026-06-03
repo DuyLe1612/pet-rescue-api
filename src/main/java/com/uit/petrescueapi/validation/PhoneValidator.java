6519bb568f7c238bc5744a8da0ad9ca6bb0b978b
duyngulam <neargamer3103@gmail.com>
2026-06-03 13:36:18 +0700

    fix: remove strict validation

--- src/main/java/com/uit/petrescueapi/validation/PhoneValidator.java
+++ src/main/java/com/uit/petrescueapi/validation/PhoneValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 public class PhoneValidator implements ConstraintValidator<Phone, String> {
 
     // Very small E.164-like validation: optional +, then country code and subscriber (max 15 digits)
-    private static final String E164_REGEX = "^\\+?[1-9][0-9]{1,14}$";
+    private static final String E164_REGEX = "^\\+?[0-9][0-9]{1,14}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
